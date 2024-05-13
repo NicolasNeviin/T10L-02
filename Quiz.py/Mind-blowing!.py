4b7b85aecd9fac9a6737ec1d6db665169e3a971b
NicolasNeviinPaul <1221108295@student.mmu.edu.my>
2024-05-13 20:45:36 +0800

    lines 1 to 32.

---
+++ Quiz.py/Mind-blowing!.py
@@ -0,0 +1,32 @@
+Quiz = input("What is red? ")
+Quiz_1 = input("What is 1 * 0? ")
+Quiz_2 =input("How are you? ")
+Quiz_3 =input("5+5? ")
+Quiz_4 =input("7+7? ")
+Quiz_5 =input("6*6? ")
+C = "Correct"
+W = "Wrong"
+if Quiz == "apple":
+    print(C)
+else:
+    print(W)
+if Quiz_1 == "0":
+    print(C)
+else:
+    print(W)
+if Quiz_2 == "ok":
+    print(C)
+else:
+    print(W)
+if Quiz_3 == "10":
+    print(C)
+else:
+    print(W)
+if Quiz_4 == "14":
+    print(C)
+else:
+    print(W)
+if Quiz_5 == "36":
+    print(C)
+else:
+    print(W)                              
