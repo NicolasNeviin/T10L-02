6da2484ccbbb9550504119d2d4ab02bb9e0af602
kehwk <167624078+kehwk@users.noreply.github.com>
2024-05-28 10:12:05 +0800

    new upload (whole folder)

---
+++ for wen kai testing/Untitled-1.py
@@ -0,0 +1,2 @@
+f = open("./peguin.png")
+f.close()
---
+++ for wen kai testing/tkinter testing.py
@@ -0,0 +1,50 @@
+from tkinter import *
+from PIL import ImageTk,Image
+import os.path
+
+os.path.abspath("./peguin.png")
+os.path.abspath("./grass-4149340_1920.png")
+os.path.abspath(r"./mindblown-mindblowing.ico")
+
+
+root = Tk()
+root.title("MindBlowing")
+root.iconbitmap("./mindblown-mindblowing.ico")
+
+
+
+
+
+pic0 = ImageTk.PhotoImage(Image.open("./peguin.png"))
+pic1 = ImageTk.PhotoImage(Image.open("./grass-4149340_1920.png"))
+bg = PhotoImage(file = "./grass-4149340_1920.png")
+mylabel = Label(root , image=bg)
+
+
+
+Imagelist = (pic0 , pic1)
+
+def myClick():
+    mylabel = Label(root,text ="I TOLD YOU DO NOT CLICK IT!!",fg="red").place(x=500 , y=150)
+
+def forward(image_number):
+    global mylabel
+    global buttonForward
+    global buttonBack
+
+    
+    
+
+def back():
+    return
+ 
+    
+mylabel1 = Label(root,text = "MindBlowing").place(x=600 , y=0)
+e = Entry(root,width=50).place(x=490 , y=30)
+mybutton1 = Button(root,text = "Don't Click" , command = myClick , fg="cyan" , bg="red").place(x=0 , y=0)
+Quitbutton = Button(root,text = "Quit this" , command=root.quit).place(x=1225 , y=0)
+buttonBack = Button(root,text = "<<" , command = back).place(x=600 , y=100)
+buttonForward = Button(root,text = ">>" , command = lambda : forward(2)).place(x=650 , y=99)
+
+root.mainloop()
+
